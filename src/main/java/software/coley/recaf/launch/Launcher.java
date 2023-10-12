80dad7c926dea5cb026fd7bc13c5ce5c14f74f57
Col-E <mcoley2@gmu.edu>
2023-10-12 14:06:47 -0400

    Add check to ensure user runs via command line

--- src/main/java/software/coley/recaf/launch/Launcher.java
+++ src/main/java/software/coley/recaf/launch/Launcher.java
@@ -3,11 +3,20 @@ package software.coley.recaf.launch;
 import picocli.CommandLine;
 import software.coley.recaf.launch.commands.Root;
 
+import javax.swing.JOptionPane;
+
+
 /**
  * Entry point class.
  */
 public class Launcher {
 	public static void main(String[] args) {
+		// Check if user tried to run by double-clicking the jar instead of running from a console.
+		if (System.console() == null && (args == null || args.length == 0)) {
+			JOptionPane.showMessageDialog(null, "The launcher is a command line application, run it from the command line");
+		}
+
+		// Run the console commands.
 		CommandLine cmd = new CommandLine(new Root());
 		cmd.execute(args);
 	}
